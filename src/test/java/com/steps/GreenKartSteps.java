e6423b21b0ca11513d84225f361603a835e4704c
Mohammed Faisal <faisalnce@gmail.com>
2023-01-23 09:46:20 -0500

    update

--- src/test/java/com/steps/GreenKartSteps.java
+++ src/test/java/com/steps/GreenKartSteps.java
@@ -88,25 +88,43 @@ public class GreenKartSteps {
 
 	@Then("^user go to cart page$")
 	public void user_go_to_cart_page() throws Throwable {
-	    
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		   wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@alt='Cart']")));
+		   driver.findElement(By.xpath("//*[@alt='Cart']")).click();
+		   driver.findElement(By.xpath("//*[contains(text(),'PROCEED TO CHECKOUT']")).click();
 	    
 	}
 
 	@Then("^user verify total price$")
 	public void user_verify_total_price() throws Throwable {
-	    
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='totAmt']"))); 
+	    String totalPriceString= driver.findElement(By.xpath("//*[@class='totAmt']")).getText();
+		Integer totalPriceInt=Integer.parseInt(totalPriceString);
+		Integer totalPriceAddition=secondPrice +thirdPrice;
+	    Assert.assertTrue("The total price is incorrect", totalPriceInt.equals(totalPriceAddition));  
 	    
 	}
 
 	@Then("^user go to next page to select country \"([^\"]*)\"$")
 	public void user_go_to_next_page_to_select_country(String arg1) throws Throwable {
-	    
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Place Order']")));  
+	    driver.findElement(By.xpath("//*[contains(text(),'Place Order']")).click();
+	    driver.findElement(By.xpath("//*[@style=\"width: 200px;\"]")).click();
+	    driver.findElement(By.xpath("//*[@value='" + arg1 + "']")).click();
+	    driver.findElement(By.xpath("//*[@type='checkbox']")).click();
+	    driver.findElement(By.xpath("//*[contains(text(),'Proceed']")).click();     
 	    
 	}
 
 	@Then("^user verify succesfully shipped items$")
 	public void user_verify_succesfully_shipped_items() throws Throwable {
-	    
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'" + arg1 + "')]")));	
+	    WebElement actualElement = driver.findElement(By.xpath("//*[contains(text(),'Thank you,your order has been placed successfully')]"));
+	    String actual = actualElement.getText();
+	    Assert.assertTrue("The text for product shipment is incorrect", actual.contains(arg1)); 
 	    
 	}
 
